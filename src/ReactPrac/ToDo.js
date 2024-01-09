0d36bcce7bd89ad75a9060c8edf0f2b07f4bdf5c
PepperDouglas <douglas.sebom@appliedtechnology.se>
2024-01-09 12:30:17 +0100

    added todo

--- src/ReactPrac/ToDo.js
+++ src/ReactPrac/ToDo.js
@@ -10,17 +10,18 @@ export default function ToDo() {
         color: #BF4F74;
         margin: 0.5em 1em;
         padding: 0.25em 1em;
+        width: 80%;
     `
 
     const ToDoDiv = styled.div`
-        min-width: 200px;
+        min-width: 49%;
         min-height: 200px;
         border: 2px solid #BF4F74;
         float: left;
     `
 
     const DoneDiv = styled.div`
-        min-width: 200px;
+        min-width: 49%;
         min-height: 200px;
         border: 2px solid #BF4F74;
         float: left;
@@ -28,53 +29,78 @@ export default function ToDo() {
 
     //First plan, have two states: todos and dones. whole..
     //This has proven difficult since deleting them relies on shared array but their pos is split
+    //Lets save having one state for a potential refactor
 
     const itemInput = useRef();
-    const [todos, updateTodos] = useState([new NoteData("Not done", false), new NoteData("Done", true)]);
+    const [todos, updateTodos] = useState([new NoteData("Handla mat", false), new NoteData("Ladda mobilen", false)]);
+    const [dones, updateDones] = useState([new NoteData("Tvätta kläder", true), new NoteData("Vattna blomman", true)]);
+
 
     const DisplayTodos = (checkval) => {
         return(
             <ol>
                 {todos.filter(u => u.IsDone === checkval).map((t, i) => 
                     <li key={i}>
-                        
-                        {GetButton(checkval, i)}
-                        
-                        
+                        {GetButton(checkval, i)}              
                         {t.Message}
                     </li>)}
             </ol>
         )
     }
 
-    const DeleteItem = () => {
-        
-        updateTodos(todos)
+    const DisplayDones = (checkval) => {
+        return(
+            <ol>
+                {dones.filter(u => u.IsDone === checkval).map((t, i) => 
+                    <li key={i}>
+                        {GetButton(checkval, i)}              
+                        {t.Message}
+                    </li>)}
+            </ol>
+        )
     }
 
-    const MakeItemDone = () => {
+    const DeleteItem = (pos) => {
+        let doneNotes = dones;
+        doneNotes.splice(pos, 1);
+        updateDones([...doneNotes]);
+    }
 
+    const MakeItemDone = (pos) => {
+        let doingNotes = todos;
+        //alert('I have pos' + pos + '\nMy mess is: ' + todos[pos].Message);
+        const noteToMove = todos[pos];
+        noteToMove.IsDone = true;
+        updateDones([...dones, noteToMove]);
+        //We cant take the return from splice, but we can use it later to refactor this code!
+        doingNotes.splice(pos, 1);
+        updateTodos([...doingNotes]);
+    }
+
+    const AddItem = () => {
+        const newNote = new NoteData(itemInput.current.value, false);
+        updateTodos([...todos, newNote]);
     }
 
     const GetButton = (checkval, i) => {
         if(!checkval){
             return(
-                <button onClick={MakeItemDone}>✓</button>
+                <button key={i} onClick={() => MakeItemDone(i)}>✓</button>
             )
         } else {
             return(
-                <button onClick={DeleteItem}>𐄂</button>
+                <button key={i} onClick={() => DeleteItem(i)}>x</button>
             )
         }
     }
 
     return(
         <Div>
-            <button>Add Item</button>
+            <button onClick={AddItem}>Add Item</button>
             <input ref={itemInput}></input>
             <br></br>
             <ToDoDiv>Things to do{DisplayTodos(false)}</ToDoDiv>
-            <DoneDiv>Done{DisplayTodos(true)}</DoneDiv>
+            <DoneDiv>Done{DisplayDones(true)}</DoneDiv>
         </Div>
     )
 
